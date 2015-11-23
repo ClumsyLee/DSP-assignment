3e872c552c6c1adc51329a51abf2898ad712555f
Thomas Lee <lisihan969@gmail.com>
2015-11-23 20:38:45 +0800

    Do not iterate

--- src/interp_temp.m
+++ src/interp_temp.m
@@ -1,51 +1,34 @@
 function temp_grid = interp_temp(pos, temp, interval, alpha, desired_stations)
-    RP_INIT = 4000;
-    ITER = 3;
-    avg_w = (1 - exp(-alpha)) / alpha - exp(1);
     len = size(pos, 1);
 
     min_xy = min(pos);
     max_xy = max(pos);
 
-    grid_size = ceil((max_xy - min_xy) / interval) + [1 1];
+    grid_size = fliplr(ceil((max_xy - min_xy) / interval) + [1 1]);
     w_mat = zeros(prod(grid_size), len);
 
     % Calculate weight matrix.
     k = 1;
-    for kx = 1:grid_size(1)
+    for kx = 1:grid_size(2)
         x = min_xy(1) + interval * (kx - 1);
-        for ky = 1:grid_size(2)
+        for ky = 1:grid_size(1)
             y = min_xy(2) + interval * (ky - 1);
-            rp = RP_INIT;
 
-            for iter = 1:ITER
-                w = weight(pos, temp, [x y], alpha, rp);
-                dp = sum(w) / (avg_w * pi * rp^2)
-                if iter == ITER
-                    n_star = desired_stations;
-                else
-                    n_star = 2 * desired_stations;
-                end
-                rp = sqrt(n_star / (dp * pi));
-            end
-
-            w_mat(k, :) = weight(pos, temp, [x y], alpha, rp)';
+            w_mat(k, :) = weight(pos, temp, [x y], alpha, desired_stations)';
             w_mat(k, :) = w_mat(k, :) / sum(w_mat(k, :));  % Normalize.
             k = k + 1;
         end
     end
-    % w_mat
 
-    %
     days = size(temp, 2);
     temp_grid = zeros([grid_size days]);
 
     for day = 1:days
-        temp_grid(:, :, day) = w_mat * temp(:, day);
+        temp_grid(:, :, day) = reshape(w_mat * temp(:, day), grid_size);
     end
 end
 
-function w = weight(pos, temp, pred_pos, alpha, rp)
+function w = weight(pos, temp, pred_pos, alpha, desired_stations)
     len = size(pos, 1);
     w = zeros(len, 1);
 
@@ -54,7 +37,9 @@ function w = weight(pos, temp, pred_pos, alpha, rp)
         r(k) = cal_dis(pos(k, :), pred_pos);
     end
 
-    non_zeros = (r <= rp);
+    [value, index] = sort(r);
+    non_zeros = index(1:desired_stations);
+    rp = value(desired_stations);
+
     w(non_zeros) = exp(-(r(non_zeros) / rp).^2 * alpha) - exp(-alpha);
-    % w
 end
--- src/run.m
+++ src/run.m
@@ -1,3 +1,9 @@
 load '../data/data.mat'
 
-grid = interp_temp(data(:, [end end-1]), data(:, 2:end-2), 10, 3.5, 10);
+data2 = data;
+for day = 1:size(data2, 2)-3
+    row = randi([1, size(data2, 1)]);
+    data2(row, day) = data2(row, day) + 20;
+end
+grid = interp_temp(data(:, [end end-1]), data(:, 2:end-2), 1, 3.5, 10);
+grid2 = interp_temp(data2(:, [end end-1]), data2(:, 2:end-2), 1, 3.5, 10);
